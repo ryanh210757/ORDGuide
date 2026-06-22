f233ad46afd2e8c43e394dafc6e06c7dfbe16745
ryanh210757 <kryanharris1@gmail.com>
2026-06-22 10:20:35 -0500

    remove JSON import

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import { Amplify } from 'aws-amplify'
-import outputs from '../amplify_outputs.json'  // Go up one level from src to root
-
-Amplify.configure(outputs)
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
